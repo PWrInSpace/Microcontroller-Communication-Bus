bb0fc2a9b1204ea3c3b2ba2a139166b10a1d168f
Michał Kowalski <248959@student.pwr.edu.pl>
2021-12-26 16:27:48 +0100

    ESP32 Slave I2C project added

---
+++ I2C_Bus/ESP_Slave_I2C/include/StructToTransmit.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT_TO_TRANSMIT_H
+#define STRUCT_TO_TRANSMIT_H
+
+#include <stdint.h>
+
+// Struct of data (eg. readings from sensors) sended from slave to this master (the same struct must be in slave device):
+
+struct StructToTransmit {
+    
+    bool ledState;
+    uint8_t lastDoneCommandNum;
+    float adcVoltage;
+    int16_t adcValue;
+};
+
+#endif
---
+++ I2C_Bus/ESP_Slave_I2C/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "StructToTransmit.h"
+
+StructToTransmit txStruct;
+
+uint16_t adcReading;
+uint8_t ledProgramm;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
